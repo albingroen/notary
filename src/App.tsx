85ed048a5937207fd69b1472aef8da3e70849289
Albin Groen <albin.groen@gmail.com>
2023-10-16 23:14:51 +0200

    Make it feel more like macOS

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import { useEffect } from 'react'
-import { getNotes } from './lib/notes'
-
 function App() {
-    useEffect(() => {
-        getNotes().then(console.log)
-    }, [])
-
     return (
-        <div>
+        <div className="pt-7">
             <p>Notary</p>
+            <button className="bg-neutral-800 hover:bg-indigo-600 duration-150 hover:text-white transition-colors px-3 py-1.5 rounded-md active:bg-indigo-800">
+                New note
+            </button>
         </div>
     )
 }
